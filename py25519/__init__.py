9d52dced7c068e2f07886e9ca3552cda6dddae5a
Jonathan <ondesmartenot@riseup.net>
2023-01-28 21:17:54 +0800

    add setup.py and __init__.py

---
+++ py25519/__init__.py
@@ -0,0 +1,70 @@
+from ctypes import cdll, CDLL, c_ulonglong, byref, create_string_buffer, util, RTLD_GLOBAL
+
+librb = CDLL(util.find_library('randombytes_kernel') or
+             util.find_library('librandombytes_kernel'),
+             mode=RTLD_GLOBAL)
+lib = CDLL(util.find_library('25519') or
+                       util.find_library('lib25519'))
+if not lib._name:
+    raise ValueError('Unable to find lib25519')
+
+
+def __check(code):
+    if code != 0:
+        raise ValueError
+
+
+lib25519_dh_SECRETKEYBYTES = 32
+lib25519_dh_PUBLICKEYBYTES = 32
+lib25519_dh_BYTES = 32
+lib25519_sign_SECRETKEYBYTES = 64
+lib25519_sign_PUBLICKEYBYTES = 32
+lib25519_sign_BYTES = 64
+
+
+def dh_keypair() -> (bytes, bytes):
+    sk = create_string_buffer(lib25519_dh_SECRETKEYBYTES)
+    pk = create_string_buffer(lib25519_dh_PUBLICKEYBYTES)
+    lib.lib25519_dh_keypair(pk, sk)
+    return pk.raw, sk.raw
+
+
+def dh(pk, sk) -> bytes:
+    if len(pk) != lib25519_dh_PUBLICKEYBYTES:
+        raise ValueError
+    if len(sk) != lib25519_dh_SECRETKEYBYTES:
+        raise ValueError
+    sk = create_string_buffer(sk)
+    pk = create_string_buffer(pk)
+    k = create_string_buffer(lib25519_dh_BYTES)
+    lib.lib25519_dh(k, pk, sk)
+    return k.raw
+
+
+def sign_keypair():
+    pk = create_string_buffer(lib25519_sign_PUBLICKEYBYTES)
+    sk = create_string_buffer(lib25519_sign_SECRETKEYBYTES)
+    lib.lib25519_sign_keypair(pk, sk)
+    return pk.raw, sk.raw
+
+
+def sign(m, sk):
+    assert len(sk) == lib25519_sign_SECRETKEYBYTES
+    mlen = c_ulonglong(len(m))
+    smlen = c_ulonglong(0)
+    sm = create_string_buffer(len(m) + lib25519_sign_BYTES)
+    m = create_string_buffer(m)
+    sk = create_string_buffer(sk)
+    lib.lib25519_sign(sm, byref(smlen), m, mlen, sk)
+    return sm.raw[:smlen.value]
+
+
+def open(sm, pk):
+    assert len(pk) == lib25519_sign_PUBLICKEYBYTES
+    smlen = c_ulonglong(len(sm))
+    m = create_string_buffer(len(sm))
+    mlen = c_ulonglong(0)
+    pk = create_string_buffer(pk)
+    code = lib.lib25519_sign_open(m, byref(mlen), sm, smlen, pk)
+    __check(code)
+    return m.raw[:mlen.value]
